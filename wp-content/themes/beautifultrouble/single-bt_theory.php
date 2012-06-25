b4f6d9259bac0f286430cc4b32a83219603269f2
Phillip Smith <ps@phillipadsmith.com>
2012-06-24 22:25:06 -0400

    Add sidebar-book to all module pages

--- wp-content/themes/beautifultrouble/single-bt_theory.php
+++ wp-content/themes/beautifultrouble/single-bt_theory.php
@@ -80,6 +80,7 @@ get_header(); ?>
 
         </div><!-- /.span8 -->
         <div id="marginalia" class="fluid-sidebar sidebar span4" role="complementary">
+            <?php get_sidebar('book'); ?>
             <?php if( $fields['repeating_elements'] ) {
                 if( $further_insights ) {
                 // Further Insights
