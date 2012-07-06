d8e9a8bafe85dfc3a4ea9732ab3667eaa2f3c4c3
Phillip Smith <ps@phillipadsmith.com>
2012-07-06 16:49:28 -0400

    Fix for full-width pages

---
+++ wp-content/themes/beautifultrouble/page-full-width.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Full-width Page
+ * Description: A full-width template with no sidebar
+ *
+ * @package WordPress
+ * @subpackage WP-Bootstrap
+ * @since WP-Bootstrap 0.1
+ */
+
+get_header(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+  <div class="row">
+  <div class="container">
+   <?php if (function_exists('bootstrapwp_breadcrumbs')) bootstrapwp_breadcrumbs(); ?>
+   </div><!--/.container -->
+   </div><!--/.row -->
+   <div class="container">
+
+      
+ <!-- Masthead
+      ================================================== -->
+      <header class="jumbotron subhead" id="overview">
+        <h1><?php the_title();?></h1>
+      </header>
+			
+				<div class="content">
+				  <?php the_content();?>
+				<?php endwhile; // end of the loop. ?>
+		
+				</div><!-- .row content -->
+		
+
+<?php get_footer(); ?>
