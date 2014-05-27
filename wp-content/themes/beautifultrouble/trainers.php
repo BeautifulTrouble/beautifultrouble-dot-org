59f30cfeb67b9e84547daa14e35c15c7cf8d6ca6
Adrian Carpenter <adriatic.c@gmail.com>
2014-05-27 00:50:11 +0000

    Fix (add) name links on trainers page template

--- wp-content/themes/beautifultrouble/trainers.php
+++ wp-content/themes/beautifultrouble/trainers.php
@@ -26,18 +26,38 @@ get_header(); ?>
             foreach($trainers as $trainer) {
                     $trainer_title = get_the_title($trainer->ID);
                     $avatar_url = get_img_url(get_the_post_thumbnail($trainer->ID, $avatar_size));
+                    $trainer_url = $secondary_url = $tertiary_url = '';
 
-                    // If there's a user with the same name as a trainer, get their avatar.
+                    // If there's a user with the same name as a trainer, get their avatar and url.
                     foreach(get_users() as $user) {
                         if ($user->display_name == $trainer_title) {
                             $avatar_url = get_img_url(get_avatar($user->ID, $avatar_size));
+                            $trainer_url = get_author_posts_url($user->ID);
+
+                            // If the user has a profile url set, use that one
+                            $secondary_url = get_userdata($user->ID)->user_url;
+                            if ($secondary_url) {
+                                $trainer_url = $secondary_url;
+                            }
                             break;
                         }
+                    }
+
+                    $tertiary_url = get_field('trainer_url', $trainer->ID);
+                    if ($tertiary_url) {
+                        $trainer_url = $tertiary_url;
                     }
 
                     echo '<div class="row spacer">';
                         echo '<div class="span2 big-avatar" style="background-image:url(\'' . $avatar_url . '\');"></div>';
                         echo '<div class="span6">';
+                            echo '<h3>';
+                            if ($trainer_url) {
+                                echo '<a href="' . $trainer_url . '">' . $trainer_title . '</a>';
+                            } else {
+                                echo $trainer_title;
+                            }
+                            echo '</h3>';
                             echo '<p>' . $trainer->post_content . '</p>';
                         echo '</div>';
                     echo '</div>';
