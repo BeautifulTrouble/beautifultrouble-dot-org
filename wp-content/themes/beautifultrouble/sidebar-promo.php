12261d596942c8335b6ff45f11715583d6b928f4
Adrian Carpenter <adriatic.c@gmail.com>
2015-03-20 06:03:07 +0000

    Revert to showing full book by default

--- wp-content/themes/beautifultrouble/sidebar-promo.php
+++ wp-content/themes/beautifultrouble/sidebar-promo.php
@@ -7,12 +7,12 @@
                                     Published by <b>OR Books</b>. Order your copy today:
                                     </p>
 <ul class="nav nav-tabs" id="editions">
-  <li><a href="#full" data-toggle="tab">Full<br />Edition</a></li>
+  <li class="active"><a href="#full" data-toggle="tab">Full<br />Edition</a></li>
   <li><a href="#pocket" data-toggle="tab">Pocket<br />Edition</a></li>
-  <li class="active" ><a href="#donate" data-toggle="tab">Gift<br />Edition</a></li>
+  <li><a href="#donate" data-toggle="tab">Gift<br />Edition</a></li>
 </ul>
 <div class="tab-content">
-  <div class="tab-pane" id="full">
+  <div class="tab-pane active" id="full">
     <img class="alignleft" src="/wp-content/themes/beautifultrouble/img/Beautiful_Trouble_Book_Cover_med.png" width="150" height="185" /><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=264"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$25<br />paperback</span><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=265"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$10<br />ebook</span><br />
@@ -24,7 +24,7 @@
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=446"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$7<br />ebook</span><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=447"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$18<br />both</span>
   </div>
-  <div class="tab-pane active" id="donate">
+  <div class="tab-pane" id="donate">
     <img class="alignleft" src="/wp-content/themes/beautifultrouble/img/Beautiful_Trouble_Book_Donate.png" width="150" height="190" />
     <span class="promo-copy">Help us get <em>Beautiful Trouble</em> into the hands that need it most! Purchase a gift copy of the book and we'll donate it to a deserving revolutionary on a tight budget.<br /><br /></span>
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=648"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$25<br />gift edition</span>
