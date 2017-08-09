b78ecd34f0a32252e3839976a61218f92af04ed1
Adrian Carpenter <adriatic.c@gmail.com>
2017-08-08 17:05:27 -0700

    Update book widget to include BR

--- wp-content/themes/beautifultrouble/sidebar-promo.php
+++ wp-content/themes/beautifultrouble/sidebar-promo.php
@@ -7,27 +7,28 @@
                                     Published by <b>OR Books</b>. Order your copy today:
                                     </p>
 <ul class="nav nav-tabs" id="editions">
-  <li class="active"><a href="#full" data-toggle="tab">Full<br />Edition</a></li>
-  <li><a href="#pocket" data-toggle="tab">Pocket<br />Edition</a></li>
-  <li><a href="#donate" data-toggle="tab">Gift<br />Edition</a></li>
+  <li class="active"><a href="#bt" data-toggle="tab">Beautiful<br />Troubl</a></li>
+  <li><a href="#bt-pocket" data-toggle="tab">BT<br />Pocket</a></li>
+  <li><a href="#rising" data-toggle="tab">Beautiful<br />Rising</a></li>
 </ul>
 <div class="tab-content">
-  <div class="tab-pane active" id="full">
+  <div class="tab-pane active" id="bt">
     <img class="alignleft" src="/wp-content/themes/beautifultrouble/img/Beautiful_Trouble_Book_Cover_med.png" width="150" height="185" /><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=264"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$25<br />paperback</span><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=265"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$10<br />ebook</span><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=266"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$28<br />both</span>
   </div>
-  <div class="tab-pane" id="pocket">
+  <div class="tab-pane" id="bt-pocket">
     <img class="alignleft" src="/wp-content/themes/beautifultrouble/img/Beautiful_Trouble_Book_Cover_PE_med.png" width="150" height="185" /><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=448"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$13<br />paperback</span><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=446"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$7<br />ebook</span><br />
     <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=447"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$18<br />both</span>
   </div>
-  <div class="tab-pane" id="donate">
-    <img class="alignleft" src="/wp-content/themes/beautifultrouble/img/Beautiful_Trouble_Book_Donate.png" width="150" height="190" />
-    <span class="promo-copy">Help us get <em>Beautiful Trouble</em> into the hands that need it most! Purchase a gift copy of the book and we'll donate it to a deserving revolutionary on a tight budget.<br /><br /></span>
-    <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=648"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$25<br />gift edition</span>
+  <div class="tab-pane" id="bt-pocket">
+    <img class="alignleft" src="/wp-content/themes/beautifultrouble/img/Beautiful_Rising_Book_Cover_med.png" width="150" height="185" /><br />
+    <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=1033"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$22<br />paperback</span><br />
+    <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=1034"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$10<br />ebook</span><br />
+    <a class="img" href="http://store-93fb1.mybigcommerce.com/cart.php?action=add&product_id=1035"><img src="http://orbooks.com/wp-content/themes/orbooks/media/image/add_button.gif" /></a><span class="price">$25<br />both</span>
   </div>
 </div>
     <p><i class="highlight">Not sure yet? <b><a href="/the-book">Find out more about the books!</a></b></i></p>
