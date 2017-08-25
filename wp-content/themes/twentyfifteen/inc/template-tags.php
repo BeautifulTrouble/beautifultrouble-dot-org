c3a956b95f3e836f83fcb1ea9e87c9acc9ac9afc
Adrian Carpenter <adriatic.c@gmail.com>
2017-08-25 03:43:58 +0000

    Attempt to update Wordpress to 4.8.1

--- wp-content/themes/twentyfifteen/inc/template-tags.php
+++ wp-content/themes/twentyfifteen/inc/template-tags.php
@@ -150,7 +150,7 @@ function twentyfifteen_categorized_blog() {
 		set_transient( 'twentyfifteen_categories', $all_the_cool_cats );
 	}
 
-	if ( $all_the_cool_cats > 1 ) {
+	if ( $all_the_cool_cats > 1 || is_preview() ) {
 		// This blog has more than 1 category so twentyfifteen_categorized_blog should return true.
 		return true;
 	} else {
@@ -240,4 +240,19 @@ function twentyfifteen_excerpt_more( $more ) {
 	return ' &hellip; ' . $link;
 }
 add_filter( 'excerpt_more', 'twentyfifteen_excerpt_more' );
+endif;
+
+if ( ! function_exists( 'twentyfifteen_the_custom_logo' ) ) :
+/**
+ * Displays the optional custom logo.
+ *
+ * Does nothing if the custom logo is not available.
+ *
+ * @since Twenty Fifteen 1.5
+ */
+function twentyfifteen_the_custom_logo() {
+	if ( function_exists( 'the_custom_logo' ) ) {
+		the_custom_logo();
+	}
+}
 endif;
